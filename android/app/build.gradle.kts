28f1733f7a4b4738cea610e580d74bf74505ad82
Bhaveshupadhyay <bhaveshupadhyay929@gmail.com>
2026-05-29 03:11:42 +0530

    remove unused files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.blinkit_money"
+    namespace = "flicktv.bhavesh.upadhyay"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.blinkit_money"
+        applicationId = "flicktv.bhavesh.upadhyay"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/blinkit_money/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.blinkit_money
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
